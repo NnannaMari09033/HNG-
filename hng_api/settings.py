77af9f4106d12024fdc25b3d461cce9981ba452e
NnannaMari09033 <nnannamari@gmail.com>
2025-01-30 06:06:56 -0500

    added a .env file and a gitgnore

--- hng_api/settings.py
+++ hng_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#z3dy)q0qnl14ksrm6b@zdo3#bc43^5v^!unll_n1)p60i$%1('
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv['ALLOWED_HOSTS', ''].split(',')
 
 
 # Application definition
